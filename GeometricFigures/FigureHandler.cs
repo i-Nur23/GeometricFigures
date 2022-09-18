c0f5ec07e4a51ca6780c758e7cdebb6fbbf42b5c
Ainur Iskhakov <94246600+i-Nur23@users.noreply.github.com>
2022-09-18 10:04:02 +0300

    Added FigureHandler class

---
+++ GeometricFigures/FigureHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricFigures
+{
+    public class FigureHandler
+    {
+        private IFigure _figure;
+        public double Square
+        {
+            get
+            {
+                return _figure.FindSquare();
+            }
+        }
+        public FigureHandler(IFigure figure)
+        {
+            _figure = figure;
+        }
+    }
+}
--- GeometricFigures/figures/Circle.cs
+++ GeometricFigures/figures/Circle.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GeometricFigures
+﻿namespace GeometricFigures
 {
     public class Circle : IFigure
     {
